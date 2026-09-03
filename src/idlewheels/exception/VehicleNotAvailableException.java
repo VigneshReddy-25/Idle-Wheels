b34dc262244fc26cb3c7759596366882988efbf3
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-03 07:38:14 +0530

    Add VehicleNotAvailableException class

---
+++ src/idlewheels/exception/VehicleNotAvailableException.java
@@ -0,0 +1,8 @@
+package idlewheels.exception;
+
+public class VehicleNotAvailableException extends Exception {
+
+    public VehicleNotAvailableException(String message) {
+        super(message);
+    }
+}
